62d1ed56e5b4b10dbdd4ac85a9a67f112b441992
michalharasim <michalharasim7@gmail.com>
2023-06-05 03:24:59 +0200

    fse

---
+++ src/main/java/com/michalharasim/githublisting/GithublistingApplication.java
@@ -0,0 +1,14 @@
+package com.michalharasim.githublisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithublistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithublistingApplication.class, args);
+		System.out.println("siema");
+	}
+
+}
