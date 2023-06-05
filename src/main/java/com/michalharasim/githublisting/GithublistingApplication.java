f993115b5d538c5c4cff2d11d427442e370673eb
michalharasim <michalharasim7@gmail.com>
2023-06-05 18:34:45 +0200

    test

--- src/main/java/com/michalharasim/githublisting/GithublistingApplication.java
+++ src/main/java/com/michalharasim/githublisting/GithublistingApplication.java
@@ -2,7 +2,7 @@ package com.michalharasim.githublisting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// test
 @SpringBootApplication
 public class GithublistingApplication {
 
