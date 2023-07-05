f10a0ca084d037ab8b8294742c0c442d37d7df21
michalharasim <mharasim74@gmail.com>
2023-07-05 18:16:54 +0200

    config + WebClient instead of RestTemplate.

--- src/main/java/com/michalharasim/githublisting/AppConfig.java
+++ src/main/java/com/michalharasim/githublisting/AppConfig.java
@@ -1,14 +1,21 @@
 package com.michalharasim.githublisting;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    Gson getGson() {
-        return new Gson();
+    WebClient getWebClient() {
+        return WebClient.create();
+    }
+
+    @Bean
+    ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
     }
 }
--- src/main/java/com/michalharasim/githublisting/GithubService.java
+++ src/main/java/com/michalharasim/githublisting/GithubService.java
@@ -1,43 +1,40 @@
 package com.michalharasim.githublisting;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.michalharasim.githublisting.model.GithubBranch;
 import com.michalharasim.githublisting.model.GithubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 @Service
 public class GithubService {
 
-    private boolean error;
     @Autowired
-    private Gson gson;
+    private WebClient webClient;
+
+    @Autowired
+    private ObjectMapper objectMapper;
     private static final String address = "https://api.github.com";
+
+
     public ArrayList<GithubRepository> getRepositories(String username) {
         try {
-            error = false;
             ArrayList<GithubRepository> allRepositories = new ArrayList<>();
-            URL url = new URL(address + "/users/" + username + "/repos");
+            String url = address + "/users/" + username + "/repos";
             String repoResponse = readSite(url);
-            if (repoResponse == null || error) {
+            if (repoResponse == null) {
                 return null;
             }
-            JsonArray jsonArray = gson.fromJson(repoResponse, JsonArray.class);
-            for (JsonElement jsonElement : jsonArray) {
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                if (jsonObject.get("fork").getAsString().contains("false")) {
-                    String name = jsonObject.get("name").getAsString();
-                    String ownerLogin = jsonObject.get("owner").getAsJsonObject().get("login").getAsString();
+            JsonNode jsonNode = objectMapper.readTree(repoResponse);
+            if (jsonNode.isArray()) {
+                for (JsonNode node : jsonNode) {
+                    String name = node.get("name").asText();
+                    String ownerLogin = node.get("owner").get("login").asText();
                     ArrayList<GithubBranch> branches = getBranches(username, name);
                     GithubRepository githubRepository = new GithubRepository(name, ownerLogin, branches);
                     allRepositories.add(githubRepository);
@@ -46,43 +43,38 @@ public class GithubService {
             return allRepositories;
         } catch (IOException e) {
             System.out.println("ERROR HAPPENED INSIDE GETREPOSITORIES");
+            return null;
         }
-        return null;
     }
 
     private ArrayList<GithubBranch> getBranches(String username, String repoName) throws IOException {
-        try {
-            error = false;
-            ArrayList<GithubBranch> allBranches = new ArrayList<>();
-            URL url = new URL(address + "/repos/" + username + "/" + repoName + "/branches");
-            String branchResponse = readSite(url);
-            JsonArray jsonArray = gson.fromJson(branchResponse, JsonArray.class);
-            for (JsonElement jsonElement : jsonArray) {
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                String branchName = jsonObject.get("name").getAsString();
-                String sha = jsonObject.get("commit").getAsJsonObject().get("sha").getAsString();
+        ArrayList<GithubBranch> allBranches = new ArrayList<>();
+        String url = address + "/repos/" + username + "/" + repoName + "/branches";
+        String branchResponse = readSite(url);
+        if (branchResponse == null) {
+            return null;
+        }
+        JsonNode jsonNode = objectMapper.readTree(branchResponse);
+        if (jsonNode.isArray()) {
+            for (JsonNode node : jsonNode) {
+                String branchName = node.get("name").asText();
+                String sha = node.get("commit").get("sha").asText();
                 allBranches.add(new GithubBranch(branchName, sha));
             }
-            return allBranches;
-        } catch (IOException e) {
-            throw new IOException(e.getMessage());
         }
+        return allBranches;
     }
 
-    private String readSite(URL url) {
+    private String readSite(String url) {
         try {
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                sb.append(line);
-            }
-            return sb.toString();
-        } catch (IOException e) {
-            error = true;
+            return webClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+        } catch (Exception e) {
+            System.out.println("No such user found.");
+            return null;
         }
-        return null;
     }
 }
