eab1153b5169968f29fc96cb66c263001f7e6a1b
michalharasim <mharasim74@gmail.com>
2023-07-31 03:18:24 +0200

    get instead of requestmapping

--- src/main/java/com/michalharasim/githublisting/controller/GithubController.java
+++ src/main/java/com/michalharasim/githublisting/controller/GithubController.java
@@ -6,10 +6,7 @@ import com.michalharasim.githublisting.model.GithubRepository;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +22,7 @@ public class GithubController {
         this.githubService = githubService;
     }
 
-    @RequestMapping("/users/{username}/repos")
+    @GetMapping("/users/{username}/repos")
     public List<GithubRepository> getAllRepos(@PathVariable String username, @RequestHeader("Accept") String header) {
         return githubService.getRepositories(username, header);
     }
